5c6352e2bf16173f0ea845b8851a045b6ce721b2
Maxime Mouchet <max@maxmouchet.com>
2020-11-25 14:36:10 +0100

    draft prober driver

---
+++ diamond_miner_core/prober.py
@@ -0,0 +1,28 @@
+from subprocess import Popen, PIPE
+
+# TODO: Raise error if the subprocess crash.
+class Prober:
+
+    def __init__(self, exe="diamond-miner-prober", options=[]):
+        self.exe = exe
+        self.options = options
+        self.process = None
+
+    def open(self):
+        self.process = Popen([self.exe, *self.options], stdin=PIPE)
+
+    def close(self):
+        self.process.stdin.close()
+        self.process.wait()
+
+    def send(self, probe):
+        s = probe.to_csv() + "\n"
+        self.process.stdin.write(s.encode("ascii"))
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+        return False
--- test_prober.py
+++ test_prober.py
@@ -1,6 +1,28 @@
-import subprocess
+from cperm import Permutation
+from diamond_miner_core.prober import Prober
+from ipaddress import ip_address
+from random import randbytes
+
+class Probe:
+    __slots__ = 'dst_addr', 'src_port', 'dst_port', 'ttl'
+
+    def __init__(self, dst_addr, src_port, dst_port, ttl):
+        self.dst_addr = dst_addr
+        self.src_port = src_port
+        self.dst_port = dst_port
+        self.ttl = ttl
+
+    def to_csv(self):
+        return f"{self.dst_addr},{self.src_port},{self.dst_port},{self.ttl}"
 
 if __name__ == "__main__":
-    proc = subprocess.Popen(["/Users/maxmouchet/Clones/github.com/dioptra-io/diamond-miner-prober/build/diamond-miner-prober", "-i", "NONE", "-o", "tmp", "-r", "100000", "-L", "trace"], stdin=subprocess.PIPE)
-    for i in range(1000):
-        proc.stdin.write('8.8.8.8,24000,33434,10\n'.encode('ascii'))
+    exe = "/Users/maxmouchet/Clones/github.com/dioptra-io/diamond-miner-prober/build/diamond-miner-prober"
+
+    with Prober(exe, ["-o", "tmp", "-r", "100000", "-L", "trace"]) as prober:
+        perm = Permutation(2**32-1, "cycle", "speck", randbytes(8))
+        for val in perm:
+            # TODO: Verify
+            prefix = val & 0xFFFFFF00
+            ttl = val & 0x0000001F
+            host = val >> 29
+            prober.send(Probe(ip_address(prefix) + host, 24000, 33434, ttl))
