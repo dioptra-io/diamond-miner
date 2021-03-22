8516057da4c561e0f70c7eea3460862a262d8f7f
Maxime Mouchet <max@maxmouchet.com>
2021-03-22 20:06:12 +0100

    project: fix build.py

--- build.py
+++ build.py
@@ -1,3 +1,5 @@
+from distutils.command.build_ext import build_ext
+
 from Cython.Build import cythonize
 from setuptools import Extension
 
@@ -14,4 +16,9 @@ extensions = [
 
 
 def build(setup_kwargs):
-    setup_kwargs.update({"ext_modules": cythonize(extensions)})
+    setup_kwargs.update(
+        {
+            "cmdclass": {"build_ext": build_ext},
+            "ext_modules": cythonize(extensions, language_level=3),
+        }
+    )
