03942d2322993fda9262be6c67677548d1e33b6c
Maxime Mouchet <max@maxmouchet.com>
2021-03-22 18:02:01 +0100

    cythonize: simplify format

--- diamond_miner/utilities/format.pyx
+++ diamond_miner/utilities/format.pyx
@@ -5,7 +5,9 @@ from libc.stdio cimport snprintf
 # https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/netinet_in.h.html
 # > INET6_ADDRSTRLEN: 46. Length of the string form for IPv6.
 cdef size_t INET6_ADDRSTRLEN = 46
-cdef size_t PROBE_STRLEN = INET6_ADDRSTRLEN + 3 * 5
+
+def format_probe(dst_addr_v6, uint16_t src_port, uint16_t dst_port, uint8_t ttl):
+    return f"{format_ipv6(dst_addr_v6)},{src_port},{dst_port},{ttl}"
 
 def format_ipv6(int_addr):
     """
@@ -15,14 +17,6 @@ def format_ipv6(int_addr):
     cdef size_t size = INET6_ADDRSTRLEN + 1
     cdef char *c_str = <char *> malloc(size * sizeof(char))
     cdef int length = format_ipv6_(int_addr, c_str, size)
-    if length < 0:
-        return None
-    return c_str[:length].decode('UTF-8')
-
-def format_probe(dst_addr_v6, uint16_t src_port, uint16_t dst_port, uint8_t ttl):
-    cdef size_t size = PROBE_STRLEN + 1
-    cdef char *c_str = <char *> malloc(size * sizeof(char))
-    cdef int length = format_probe_(dst_addr_v6, src_port, dst_port, ttl, c_str, size)
     if length < 0:
         return None
     return c_str[:length].decode('UTF-8')
@@ -38,9 +32,3 @@ cdef inline int format_ipv6_(int_addr, char* c_str, size_t size):
     g = (int_addr & 0x000000000000000000000000FFFF0000) >> 16
     h = int_addr & 0x0000000000000000000000000000FFFF
     return snprintf(c_str, size, "%X:%X:%X:%X:%X:%X:%X:%X", a, b, c, d, e, f, g, h)
-
-cdef inline int format_probe_(dst_addr_v6, uint16_t src_port, uint16_t dst_port, uint8_t ttl, char* c_str, size_t size):
-    cdef int length = 0
-    length += format_ipv6_(dst_addr_v6, c_str, size)
-    length += snprintf(c_str + length, size - length, ",%u,%u,%u", src_port, dst_port, ttl)
-    return length
