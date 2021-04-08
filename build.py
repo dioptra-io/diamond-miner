19c0be9987eda2c4ed200e151003d47bd8ed449d
Maxime Mouchet <max@maxmouchet.com>
2021-04-08 13:41:54 +0200

    build.py: simplify

--- build.py
+++ build.py
@@ -1,28 +1,12 @@
 from distutils.command.build_ext import build_ext
 
 from Cython.Build import cythonize
-from setuptools import Extension
-
-extensions = [
-    Extension(
-        "diamond_miner.mappers",
-        ["diamond_miner/mappers.pyx"],
-    ),
-    Extension(
-        "diamond_miner.utilities.format",
-        ["diamond_miner/utilities/format.pyx"],
-    ),
-    Extension(
-        "diamond_miner.utilities.parameter_grid",
-        ["diamond_miner/utilities/parameter_grid.pyx"],
-    ),
-]
 
 
 def build(setup_kwargs):
     setup_kwargs.update(
         {
             "cmdclass": {"build_ext": build_ext},
-            "ext_modules": cythonize(extensions, language_level=3),
+            "ext_modules": cythonize("diamond_miner/**/*.pyx", language_level=3),
         }
     )
