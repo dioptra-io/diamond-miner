062a45a57cf9217a382695a08aca8679dae8abd6
Maxime Mouchet <max@maxmouchet.com>
2021-06-04 14:06:52 +0200

    queries: more next round optimizations

--- diamond_miner/queries/get_next_round.py
+++ diamond_miner/queries/get_next_round.py
@@ -16,7 +16,7 @@ class GetNextRound(Query):
 
     Row = namedtuple(
         "Row",
-        "protocol,dst_prefix,ttls,probes,prev_max_flow",
+        "protocol,dst_prefix,prev_max_flow,probes,ttls",
     )
 
     def query(self, table: str, subset: IPNetwork = UNIVERSE_SUBSET) -> str:
@@ -35,8 +35,8 @@ class GetNextRound(Query):
 
         if self.dminer_lite:
             dm_fragment = """
-            arrayMap(n -> nks_curr[n + 1], links_per_ttl_curr) AS nkv_Dhv_curr,
-            arrayMap(n -> nks_prev[n + 1], links_per_ttl_prev) AS nkv_Dhv_prev,
+            arrayMap(k -> toUInt32(ceil(ln(epsilon_curr / (k + 1)) / ln((k + 1 - 1) / (k + 1)))), links_per_ttl_curr) AS nkv_Dhv_curr,
+            arrayMap(k -> toUInt32(ceil(ln(epsilon_prev / (k + 1)) / ln((k + 1 - 1) / (k + 1)))), links_per_ttl_prev) AS nkv_Dhv_prev,
             """
         else:
             # TODO: Implement by computing Dh(v)
@@ -48,19 +48,21 @@ class GetNextRound(Query):
         return f"""
         WITH
             {self.round_leq} AS current_round,
-            range(1, 256) AS ks,  -- Values of `k` used to compute MDA stopping points `n_k`.
             -- 1) Compute the links
             --  x.1       x.2        x.3
             -- (near_ttl, near_addr, far_addr)
             groupUniqArray((near_ttl, near_addr, far_addr)) AS links_curr,
             groupUniqArrayIf((near_ttl, near_addr, far_addr), near_round < current_round) AS links_prev,
             -- 2) Count the number of links per TTL
+            -- extract only the TTLs, this greatly speeds-up arrayCount
+            arrayMap(x -> x.1, links_curr) AS TTLs_curr,
+            arrayMap(x -> x.1, links_prev) AS TTLs_prev,
             -- find the min/max TTLs
             -- we add +2 since range() is exclusive and that we compute the max over the *near* TTL
-            range(arrayMin(x -> x.1, links_curr), arrayMax(x -> x.1, links_curr) + 2) AS TTLs,
+            range(arrayMin(TTLs_curr), arrayMax(TTLs_curr) + 2) AS TTLs,
             -- count distinct links per TTL
-            arrayMap(t -> arrayCount(x -> x.1 = t, links_curr), TTLs) AS links_per_ttl_curr,
-            arrayMap(t -> arrayCount(x -> x.1 = t, links_prev), TTLs) AS links_per_ttl_prev,
+            arrayMap(t -> countEqual(TTLs_curr, t), TTLs) AS links_per_ttl_curr,
+            arrayMap(t -> countEqual(TTLs_prev, t), TTLs) AS links_per_ttl_prev,
             -- find the maximum number of links over all TTLs
             arrayMax(links_per_ttl_curr) AS max_links_curr,
             arrayMax(links_per_ttl_prev) AS max_links_prev,
@@ -71,9 +73,6 @@ class GetNextRound(Query):
             -- epsilon (MDA probability of missing links)
             {self.target_epsilon} AS target_epsilon,
             {eps_fragment}
-            -- Compute `n_k` for each `k`
-            arrayMap(k -> toUInt32(ceil(ln(epsilon_curr / k) / ln((k - 1) / k))), ks) AS nks_curr,
-            arrayMap(k -> toUInt32(ceil(ln(epsilon_prev / k) / ln((k - 1) / k))), ks) AS nks_prev,
             -- 6) Compute the number of probes to send during the next round
             {dm_fragment}
             -- compute the number of probes sent at previous round
@@ -88,20 +87,20 @@ class GetNextRound(Query):
             -- number of probes sent during the previous rounds.
             -- if TTL > 1: we take the max of probes to send over TTL t and t-1
             arrayMap(t -> if(t == 1, nkv_Dhv_curr[t] - prev_max_flow_per_ttl[t], arrayMax([nkv_Dhv_curr[t] - prev_max_flow_per_ttl[t], nkv_Dhv_curr[t-1] - prev_max_flow_per_ttl[t-1]])), TTLs) AS candidate_probes,
-            arrayMap(t -> if(candidate_probes[t] < 0, 0, candidate_probes[t]), TTLs) AS probes,
+            arrayMap(t -> if(candidate_probes[t] < 0, 0, candidate_probes[t]), TTLs) AS probes
             -- TODO: Cleanup/optimize/rewrite/... below
             -- do not send probes to TTLs where no replies have been received
             -- it is unlikely that we will discover more at this TTL if the first 6 flows have seen nothing
-            arrayMap(t -> arrayUniq(arrayFilter(x -> x != toIPv6('::'), arrayMap(x -> x.2, arrayFilter(x -> x.1 == t, links_curr)))), TTLs) AS nodes_per_ttl_near,
-            arrayMap(t -> arrayUniq(arrayFilter(x -> x != toIPv6('::'), arrayMap(x -> x.3, arrayFilter(x -> x.1 + 1 == t, links_curr)))), TTLs) AS nodes_per_ttl_far,
-            arrayMap(t -> arrayMax([nodes_per_ttl_near[t], nodes_per_ttl_far[t]]), TTLs) AS nodes_per_ttl,
-            arrayMap(t -> if(nodes_per_ttl[t] > 0, probes[t], 0), TTLs) AS probes_final
+            -- arrayMap(t -> arrayUniq(arrayFilter(x -> x != toIPv6('::'), arrayMap(x -> x.2, arrayFilter(x -> x.1 == t, links_curr)))), TTLs) AS nodes_per_ttl_near,
+            -- arrayMap(t -> arrayUniq(arrayFilter(x -> x != toIPv6('::'), arrayMap(x -> x.3, arrayFilter(x -> x.1 + 1 == t, links_curr)))), TTLs) AS nodes_per_ttl_far,
+            -- arrayMap(t -> arrayMax([nodes_per_ttl_near[t], nodes_per_ttl_far[t]]), TTLs) AS nodes_per_ttl,
+            -- arrayMap(t -> if(nodes_per_ttl[t] > 0, probes[t], 0), TTLs) AS probes_final
         SELECT
             probe_protocol,
             probe_dst_prefix,
-            TTLs,
-            probes_final,
-            prev_max_flow_per_ttl
+            prev_max_flow_per_ttl,
+            probes,
+            TTLs
         FROM {table}
         WHERE near_round <= current_round
           AND NOT is_inter_round
@@ -109,5 +108,5 @@ class GetNextRound(Query):
           -- TODO: Temporary
           AND NOT is_partial
         GROUP BY (probe_protocol, probe_src_addr, probe_dst_prefix)
-        HAVING length(links_curr) >= 1 AND NOT skip_prefix
+        HAVING NOT skip_prefix
         """
