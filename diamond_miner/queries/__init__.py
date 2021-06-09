02932fe94b3ed989dbac49c0a8fbb49e5a39a7cd
Maxime Mouchet <max@maxmouchet.com>
2021-06-09 16:54:05 +0200

    queries: export DropTables and InsertPrefixes

--- diamond_miner/queries/__init__.py
+++ diamond_miner/queries/__init__.py
@@ -6,6 +6,7 @@ from .create_links_table import CreateLinksTable
 from .create_prefixes_table import CreatePrefixesTable
 from .create_results_table import CreateResultsTable
 from .create_tables import CreateTables
+from .drop_tables import DropTables
 from .get_invalid_prefixes import GetPrefixesWithAmplification, GetPrefixesWithLoops
 from .get_links import GetLinks
 from .get_links_from_view import GetLinksFromView
@@ -13,6 +14,7 @@ from .get_max_ttl import GetMaxTTL
 from .get_next_round import GetNextRound
 from .get_nodes import GetNodes
 from .insert_links import InsertLinks
+from .insert_prefixes import InsertPrefixes
 from .query import (
     AddrType,
     Query,
@@ -33,6 +35,7 @@ __all__ = (
     "CreatePrefixesTable",
     "CreateResultsTable",
     "CreateTables",
+    "DropTables",
     "GetLinks",
     "GetLinksFromView",
     "GetMaxTTL",
@@ -41,6 +44,7 @@ __all__ = (
     "GetPrefixesWithAmplification",
     "GetPrefixesWithLoops",
     "InsertLinks",
+    "InsertPrefixes",
     "Query",
     "flows_table",
     "links_table",
