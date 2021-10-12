217f0d5a74ee036f8ff844837dff7738c636f926
Maxime Mouchet <max@maxmouchet.com>
2021-10-12 11:37:38 +0200

    queries: add storage_policy parameter to CreateTables

--- diamond_miner/queries/create_tables.py
+++ diamond_miner/queries/create_tables.py
@@ -1,4 +1,4 @@
-from dataclasses import asdict, dataclass
+from dataclasses import dataclass, fields
 from typing import Sequence
 
 from diamond_miner.defaults import (
@@ -12,7 +12,7 @@ from diamond_miner.queries import (
     CreatePrefixesTable,
     CreateResultsTable,
 )
-from diamond_miner.queries.query import Query
+from diamond_miner.queries.query import Query, StoragePolicy
 from diamond_miner.typing import IPNetwork
 
 
@@ -22,18 +22,20 @@ class CreateTables(Query):
 
     prefix_len_v4: int = DEFAULT_PREFIX_LEN_V4
     prefix_len_v6: int = DEFAULT_PREFIX_LEN_V6
+    storage_policy: StoragePolicy = StoragePolicy()
 
     def statements(
         self, measurement_id: str, subset: IPNetwork = UNIVERSE_SUBSET
     ) -> Sequence[str]:
+        all_params = {field.name: getattr(self, field.name) for field in fields(self)}
         # Only CreateResultsTable accepts these parameters.
         params = {
             x: y
-            for x, y in asdict(self).items()
+            for x, y in all_params.items()
             if x not in ["prefix_len_v4", "prefix_len_v6"]
         }
         return (
-            *CreateResultsTable(**asdict(self)).statements(measurement_id, subset),
+            *CreateResultsTable(**all_params).statements(measurement_id, subset),
             *CreateFlowsView(**params).statements(measurement_id, subset),
             *CreateLinksTable(**params).statements(measurement_id, subset),
             *CreatePrefixesTable(**params).statements(measurement_id, subset),
