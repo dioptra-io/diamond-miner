526c1086ebe267ce446bfd1fd10578ceb7ac4a53
Maxime Mouchet <max@maxmouchet.com>
2021-11-10 16:24:38 +0100

    release(project): 0.3.2 → 0.3.3

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, dioptra.io"
 author = "dioptra.io"
 
 # Managed by bumpversion
-version = "0.3.2"
+version = "0.3.3"
 release = version
 
 # -- General configuration ---------------------------------------------------
