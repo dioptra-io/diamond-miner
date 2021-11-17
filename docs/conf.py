cbacdad976b6d047392fe3e5fa9e8290ed0dc17f
matthieugouel <matthieu.gouel@protonmail.com>
2021-11-17 18:46:21 +0100

    release(project): 0.3.3 → 0.3.4

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, dioptra.io"
 author = "dioptra.io"
 
 # Managed by bumpversion
-version = "0.3.3"
+version = "0.3.4"
 release = version
 
 # -- General configuration ---------------------------------------------------
