d17dc1af3d945a17bd71a3bad64640da74f23da9
Maxime Mouchet <max@maxmouchet.com>
2022-02-11 20:25:09 +0100

    release(project): 0.7.2 → 0.7.3

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, dioptra.io"
 author = "dioptra.io"
 
 # Managed by bumpversion
-version = "0.7.2"
+version = "0.7.3"
 release = version
 
 # -- General configuration ---------------------------------------------------
