52f23ad48124ac546cbd6d9abd8027e6ba7d6003
Maxime Mouchet <max@maxmouchet.com>
2022-07-02 11:37:57 +0200

    release(project): 0.10.0 → 0.10.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Sorbonne Université"
 author = "Sorbonne Université"
 
 # Managed by bumpversion
-version = "0.10.0"
+version = "0.10.1"
 release = version
 
 # -- General configuration ---------------------------------------------------
